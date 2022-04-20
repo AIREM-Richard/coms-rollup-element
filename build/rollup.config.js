a08dbfad4a87ae2cfc373229737e7e5f9017dfed
俞新雨 <yuxinyu@yuxinyudeMBP.lan>
2022-04-20 18:02:56 +0800

    feat(rollup.config.js): 打包优化
    使用@rollup/plugin-strip 进行去除console.log等函数，rollup-plugin-terser 插件压缩代码

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -6,6 +6,8 @@ import postcss from 'rollup-plugin-postcss';
 import postcssImport from 'postcss-import';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import strip from '@rollup/plugin-strip';
 
 const file = type => `dist/${name}.${type}.js`;
 
@@ -33,6 +35,12 @@ export default {
     }),
     commonjs({
       include: ['node_modules/**', 'node_modules/**/*'],
+    }),
+    // 压缩代码
+    terser(),
+    // 剔除debugger、assert.equal和 console.log 类似的函数
+    strip({
+      labels: ['unittest'],
     }),
   ],
   external: ['vue'],
